b43c74fe8f31b11f769db08ef11c560e9a6e44a5
Jarid Margolin <jaridmargolin@gmail.com>
2016-10-28 17:49:19 -0400

    v0.0.1

---
+++ README.md
@@ -0,0 +1,48 @@
+# inspect-process [![Build Status](https://travis-ci.org/jaridmargolin/inspect-process.png)](https://travis-ci.org/jaridmargolin/inspect-process)
+
+Dead simple debugging for node.js using chrome-devtools.
+
+Node added support for v8-inspector in [v6.3.0](https://github.com/nodejs/node/blob/master/doc/changelogs/CHANGELOG_V6.md#6.3.0) but launching the debugger for a given process is still a little clumsy. **inspect-process** is a small wrapper built on the shoulders of giants to make working with the v8-inspector dead simple.
+
+`npm install -g inspect-process`
+
+
+## Usage
+
+**Inspecting a file**
+
+```
+$ inspect [PATH_TO_FILE]
+```
+
+**Inspecting an executable**
+
+*smart enough to debug executables (ex: `inspect mocha`)*
+
+```
+$ inspect [EXECUTABLE]
+```
+
+## Tests
+
+**Install Dependencies**
+
+```
+npm install
+```
+
+**Run**
+
+```
+npm test
+```
+
+## License
+
+The MIT License (MIT) Copyright (c) 2016 Jarid Margolin
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ tests/index.js
@@ -0,0 +1,122 @@
+'use strict';
+
+/* -----------------------------------------------------------------------------
+ * dependencies
+ * ---------------------------------------------------------------------------*/
+
+// core
+const spawn = require('child_process').spawn;
+const path = require('path');
+
+// 3rd party
+const _ = require('lodash');
+const assert = require('chai').assert;
+const stdout = require('test-console').stdout;
+const stderr = require('test-console').stderr;
+
+// lib
+const inspect = require('../lib/index');
+
+
+/* -----------------------------------------------------------------------------
+ * reusable
+ * ---------------------------------------------------------------------------*/
+
+const executablePath = path.resolve(__dirname, '..', 'bin', 'inspect.js');
+const fixturesPath = path.resolve(__dirname, 'fixtures');
+
+const successPath = path.resolve(fixturesPath, 'success');
+const errorPath = path.resolve(fixturesPath, 'error');
+
+
+/* -----------------------------------------------------------------------------
+ * test
+ * ---------------------------------------------------------------------------*/
+
+describe('inspect', function () {
+
+  before(function () {
+    process.env['PATH'] += ':' + fixturesPath;
+  });
+
+  beforeEach(function () {
+    this.stdout = stdout.inspect();
+    this.stderr = stderr.inspect();
+  });
+
+  afterEach(function () {
+    this.stdout.restore();
+    this.stderr.restore();
+
+    // mocha results are printed to stdout
+    process.stdout.write(_.last(this.stdout.output));
+  });
+
+  describe('api', function () {
+
+    it('Should resolve with success.', function () {
+      return inspect(successPath);
+    });
+
+    it('Should reject with error.', function () {
+      return inspect(errorPath)
+        .then(() => { throw new Error('Promise was resolved') })
+        .catch(() => true);
+    });
+
+    it('Should resolve files found in path.', function () {
+      return inspect('success');
+    });
+
+    it('Should hide --inspect generated stderr.', function () {
+      return inspect(successPath)
+        .then(() => assert.equal(this.stderr.output.length, 0));
+    });
+
+    it('Should forward child process stderr.', function () {
+      return inspect(errorPath)
+        .then(() => { throw new Error('Promise was resolved') })
+        .catch(() => assert.equal(this.stderr.output.length, 1));
+    });
+
+    it('Should forward all stdout text.', function () {
+      return inspect(successPath)
+        .then(() => assert.equal(this.stdout.output.length, 1));
+    });
+
+  });
+
+  describe('executable', function () {
+
+    it('Should return with the child process success exitcode.', function (done) {
+      const proc = spawn(executablePath, ['success']);
+
+      proc.on('exit', () => {
+        assert.equal(proc.exitCode, 0);
+        done();
+      });
+    });
+
+    it('Should return with the child process error exitcode.', function (done) {
+      const proc = spawn(executablePath, ['error']);
+
+      proc.on('exit', () => {
+        assert.equal(proc.exitCode, 1);
+        done();
+      });
+    });
+
+    it('Should forward args.', function (done) {
+      const proc = spawn(executablePath, ['success', 'overwrite']);
+      let output;
+
+      proc.stdout.on('data', (data) => output = data.toString());
+      proc.on('exit', () => {
+        assert.equal(output, 'overwrite');
+        done();
+      });
+    });
+
+  });
+
+});
