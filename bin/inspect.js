eac83fb226c9a01a0675c955097789f695e32993
Jarid Margolin <jaridmargolin@gmail.com>
2016-11-05 19:26:23 -0400

    Update signature of inspect function to take an options object.

--- bin/inspect.js
+++ bin/inspect.js
@@ -18,21 +18,18 @@ const inspect = require('../lib/index');
 
 const args = process.argv.slice(2);
 const dividerIndex = args.indexOf('--');
-
+const options = {};
 let cmd;
-let nodeArgs;
-let childArgs;
 
 if (dividerIndex === -1) {
   cmd = args[0]
-  nodeArgs = [];
-  childArgs = args.slice(1);
+  options['childArgs'] = args.slice(1);
 } else {
   cmd = args[dividerIndex + 1];
-  nodeArgs = args.slice(0, dividerIndex);
-  childArgs = args.slice(dividerIndex + 2);
+  options['nodeArgs'] = args.slice(0, dividerIndex);
+  options['childArgs'] = args.slice(dividerIndex + 2);
 }
 
-inspect(cmd, nodeArgs, childArgs)
+inspect(cmd, options)
   .then(() => process.exit())
   .catch(() => process.exit(1));
