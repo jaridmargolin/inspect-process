e517b48371fde39c3baf52b29902d87bd3a046db
Jarid Margolin <jaridmargolin@gmail.com>
2016-12-16 20:14:09 -0500

    Use standard coding style.

--- bin/inspect.js
+++ bin/inspect.js
@@ -1,22 +1,21 @@
 #!/usr/bin/env node
-'use strict';
+'use strict'
 
 /* -----------------------------------------------------------------------------
  * dependencies
- * ---------------------------------------------------------------------------*/
+ * -------------------------------------------------------------------------- */
 
 // 3rd party
-const _ = require('lodash/fp');
-const yargs = require('yargs');
-const v8flags = require('v8flags');
+const _ = require('lodash/fp')
+const yargs = require('yargs')
+const v8flags = require('v8flags')
 
 // lib
-const inspect = require('../lib/index');
-
+const inspect = require('../lib/index')
 
 /* -----------------------------------------------------------------------------
  * usage
- * ---------------------------------------------------------------------------*/
+ * -------------------------------------------------------------------------- */
 
 const inspectCliOptions = {
   'debug-exception': {
@@ -29,69 +28,68 @@ const inspectCliOptions = {
     choices: ['silly', 'verbose', 'info'],
     default: 'info'
   }
-};
+}
 
 // early parse in order to show inspect specific help options
 yargs.options(inspectCliOptions)
   .usage('\nUsage:\ninspect [inspect options] [node options] [v8 options] [script] [arguments]')
   .version()
   .help()
-  .argv;
-
+  .argv
 
 /* -----------------------------------------------------------------------------
  * inspect
- * ---------------------------------------------------------------------------*/
+ * -------------------------------------------------------------------------- */
 
 v8flags((err, result) => {
   if (err) {
-    throw new Error(err);
+    throw new Error(err)
   }
 
-  const v8Flags = _.map((flag) => flag.substring(2))(result);
+  const v8Flags = _.map((flag) => flag.substring(2))(result)
   const nodeFlags = ['preserve-symlinks', 'zero-fill-buffers', 'prof-process',
     'track-heap-objects', 'trace-sync-io', 'trace-warnings', 'no-warnings',
     'throw-deprecation', 'trace-deprecation', 'no-deprecation', 'interactive',
-    'enable-fips', 'force-fips', 'debug-brk'];
+    'enable-fips', 'force-fips', 'debug-brk']
   const nodeStringOptions = ['require', 'eval', 'print', 'icu-data-dir=dir',
-  'openssl-config=path', 'tls-cipher-list=val'];
-  const nodeNumberOptions = ['v8-pool-size'];
+    'openssl-config=path', 'tls-cipher-list=val']
+  const nodeNumberOptions = ['v8-pool-size']
 
   const parsed = yargs
     .boolean(v8Flags)
     .boolean(nodeFlags)
     .string(nodeStringOptions)
     .number(nodeNumberOptions)
-    .argv;
+    .argv
 
-  const args = process.argv.slice(2);
-  const cmd = parsed._[0];
-  const cmdIndex = args.indexOf(cmd);
-  const processArgs = args.slice(0, cmdIndex);
+  const args = process.argv.slice(2)
+  const cmd = parsed._[0]
+  const cmdIndex = args.indexOf(cmd)
+  const processArgs = args.slice(0, cmdIndex)
 
   // all keys after the cmd should be considered childArgs
-  const childArgs = args.slice(cmdIndex + 1);
-  const childFlags = _.map((arg) => arg.split('=')[0])(childArgs);
+  const childArgs = args.slice(cmdIndex + 1)
+  const childFlags = _.map((arg) => arg.split('=')[0])(childArgs)
 
   // inspectOptions are just picked from our parsed args. We pass "options"
   // rather than args because we are not proxying the args to the future
   // child_process
-  const inspectKeys = _.keys(inspectCliOptions);
-  const inspectFlags = _.map((key) => '--' + key)(inspectKeys);
+  const inspectKeys = _.keys(inspectCliOptions)
+  const inspectFlags = _.map((key) => '--' + key)(inspectKeys)
   const inspectOptions = _.compose(_.omitBy((val, key) => {
     return childFlags.includes('--' + key)
-  }), _.pick(inspectKeys))(parsed);
+  }), _.pick(inspectKeys))(parsed)
 
   // node args are simply processArgs that are not inspectArgs
   const nodeArgs = _.remove((arg) => {
-    return inspectFlags.includes(arg.split('=')[0]);
-  })(processArgs);
+    return inspectFlags.includes(arg.split('=')[0])
+  })(processArgs)
 
   inspect(cmd, {
-      nodeArgs: nodeArgs,
-      childArgs: childArgs,
-      inspectOptions: inspectOptions
-    })
+    nodeArgs: nodeArgs,
+    childArgs: childArgs,
+    inspectOptions: inspectOptions
+  })
     .then(() => process.exit())
-    .catch(() => process.exit(1));
-});
+    .catch(() => process.exit(1))
+})
--- test/index.js
+++ test/index.js
@@ -1,205 +1,193 @@
-'use strict';
+/* eslint-env mocha */
+'use strict'
 
 /* -----------------------------------------------------------------------------
  * dependencies
- * ---------------------------------------------------------------------------*/
+ * -------------------------------------------------------------------------- */
 
 // core
-const spawn = require('child_process').spawn;
-const path = require('path');
-const net = require('net');
+const spawn = require('child_process').spawn
+const path = require('path')
+const net = require('net')
 
 // 3rd party
-const _ = require('lodash/fp');
-const assert = require('chai').assert;
-const stdout = require('test-console').stdout;
-const stderr = require('test-console').stderr;
+const _ = require('lodash/fp')
+const assert = require('chai').assert
+const stdout = require('test-console').stdout
+const stderr = require('test-console').stderr
 
 // lib
-const inspect = require('../lib/index');
-
+const inspect = require('../lib/index')
 
 /* -----------------------------------------------------------------------------
  * reusable
- * ---------------------------------------------------------------------------*/
+ * -------------------------------------------------------------------------- */
 
-const executablePath = path.resolve(__dirname, '..', 'bin', 'inspect.js');
-const fixturesPath = path.resolve(__dirname, 'fixtures');
+const executablePath = path.resolve(__dirname, '..', 'bin', 'inspect.js')
+const fixturesPath = path.resolve(__dirname, 'fixtures')
 
-const successPath = path.resolve(fixturesPath, 'success');
-const errorPath = path.resolve(fixturesPath, 'error');
-const exceptionPath = path.resolve(fixturesPath, 'exception');
-
+const successPath = path.resolve(fixturesPath, 'success')
+const errorPath = path.resolve(fixturesPath, 'error')
+const exceptionPath = path.resolve(fixturesPath, 'exception')
 
 /* -----------------------------------------------------------------------------
  * test
- * ---------------------------------------------------------------------------*/
+ * -------------------------------------------------------------------------- */
 
 describe('inspect', function () {
-
   before(function () {
-    process.env['PATH'] += ':' + fixturesPath;
-  });
+    process.env['PATH'] += ':' + fixturesPath
+  })
 
   beforeEach(function () {
-    this.stdout = stdout.inspect();
-    this.stderr = stderr.inspect();
-  });
+    this.stdout = stdout.inspect()
+    this.stderr = stderr.inspect()
+  })
 
   afterEach(function () {
-    this.stdout.restore();
-    this.stderr.restore();
+    this.stdout.restore()
+    this.stderr.restore()
 
     // mocha results are printed to stdout
-    process.stdout.write(_.last(this.stdout.output));
-  });
-
+    process.stdout.write(_.last(this.stdout.output))
+  })
 
   /* ---------------------------------------------------------------------------
    * api
-   * -------------------------------------------------------------------------*/
+   * ------------------------------------------------------------------------ */
 
   describe('api', function () {
-
     it('Should resolve with success.', function () {
-      return inspect(successPath);
-    });
+      return inspect(successPath)
+    })
 
     it('Should reject with error.', function () {
       return inspect(errorPath)
         .then(() => { throw new Error('Promise was resolved') })
-        .catch(() => true);
-    });
+        .catch(() => true)
+    })
 
     it('Should resolve files found in path.', function () {
-      return inspect('success');
-    });
+      return inspect('success')
+    })
 
     it('Should hide --inspect generated stderr.', function () {
       return inspect(successPath)
-        .then(() => assert.equal(this.stderr.output.length, 0));
-    });
+        .then(() => assert.equal(this.stderr.output.length, 0))
+    })
 
     it('Should forward child process stderr.', function () {
       return inspect(errorPath)
         .then(() => { throw new Error('Promise was resolved') })
-        .catch(() => assert.equal(this.stderr.output.length, 1));
-    });
+        .catch(() => assert.equal(this.stderr.output.length, 1))
+    })
 
     it('Should forward all stdout text.', function () {
       return inspect(successPath)
-        .then(() => assert.equal(this.stdout.output.length, 1));
-    });
+        .then(() => assert.equal(this.stdout.output.length, 1))
+    })
 
     it('Should find an open port.', function (done) {
-      const server = net.createServer();
-      server.listen(9229, () => inspect(successPath).then(done));
-    });
+      const server = net.createServer()
+      server.listen(9229, () => inspect(successPath).then(done))
+    })
 
     it('Should use path to local chromedriver instance.', function () {
-      const paths = process.env['PATH'].split(':');
+      const paths = process.env['PATH'].split(':')
       const removeChromeDriver = _.remove((key) => {
         return key.includes('chromedriver') ||
-          key.includes('inspect-process/node_modules/.bin');
-      });
+          key.includes('inspect-process/node_modules/.bin')
+      })
 
-      process.env['PATH'] = removeChromeDriver(paths).join(':');
-      return inspect(successPath);
-    });
+      process.env['PATH'] = removeChromeDriver(paths).join(':')
+      return inspect(successPath)
+    })
 
     it('Should respect --debug-brk nodeArg.', function (done) {
       const inspected = inspect(successPath, {
         nodeArgs: ['--debug-brk']
-      });
+      })
 
       inspected.devtools.onOpen = function () {
         inspected.devtools._waitUntilPause()
           .then(() => inspected.devtools._continueExecution())
           .then(() => done())
-      };
-    });
-
-  });
-
+      }
+    })
+  })
 
   /* ---------------------------------------------------------------------------
    * options
-   * -------------------------------------------------------------------------*/
+   * ------------------------------------------------------------------------ */
 
   describe('options', function () {
-
     it('Should debug exceptions (--debug-exception).', function (done) {
       const inspected = inspect(exceptionPath, {
         inspectOptions: { 'debug-exception': true }
-      });
+      })
 
       inspected.devtools.onOpen = function () {
         inspected.devtools._waitUntilPause()
           .then(() => inspected.devtools._continueExecution())
-          .then(() => done());
-      };
-    });
-
-  });
-
+          .then(() => done())
+      }
+    })
+  })
 
   /* ---------------------------------------------------------------------------
    * executable
-   * -------------------------------------------------------------------------*/
+   * ------------------------------------------------------------------------ */
 
   describe('executable', function () {
-
     it('Should return with the child process success exitcode.', function (done) {
-      const proc = spawn(executablePath, ['success']);
+      const proc = spawn(executablePath, ['success'])
 
       proc.on('exit', () => {
-        assert.equal(proc.exitCode, 0);
-        done();
-      });
-    });
+        assert.equal(proc.exitCode, 0)
+        done()
+      })
+    })
 
     it('Should return with the child process error exitcode.', function (done) {
-      const proc = spawn(executablePath, ['error']);
+      const proc = spawn(executablePath, ['error'])
 
       proc.on('exit', () => {
-        assert.equal(proc.exitCode, 1);
-        done();
-      });
-    });
+        assert.equal(proc.exitCode, 1)
+        done()
+      })
+    })
 
     it('Should forward childArgs.', function (done) {
-      const proc = spawn(executablePath, ['success', 'overwrite']);
-      let output;
+      const proc = spawn(executablePath, ['success', 'overwrite'])
+      let output
 
-      proc.stdout.on('data', (data) => output = data.toString());
+      proc.stdout.on('data', (data) => (output = data.toString()))
       proc.on('exit', () => {
-        assert.equal(output, 'overwrite');
-        done();
-      });
-    });
+        assert.equal(output, 'overwrite')
+        done()
+      })
+    })
 
     it('Should forward nodeArgs', function (done) {
-      const proc = spawn(executablePath, ['--require', './test/fixtures/required', 'success']);
-      let output = '';
+      const proc = spawn(executablePath, ['--require', './test/fixtures/required', 'success'])
+      let output = ''
 
-      proc.stdout.on('data', (data) => output = data.toString());
+      proc.stdout.on('data', (data) => (output = data.toString()))
       proc.on('exit', () => {
-        assert.equal(output, 'required');
-        done();
-      });
-    });
+        assert.equal(output, 'required')
+        done()
+      })
+    })
 
     it('Should forward inspectArgs', function (done) {
-      const proc = spawn(executablePath, ['--log-level=verbose', 'success']);
-      let output = '';
+      const proc = spawn(executablePath, ['--log-level=verbose', 'success'])
+      let output = ''
 
-      proc.stderr.on('data', (data) => output = data.toString());
+      proc.stderr.on('data', (data) => (output = data.toString()))
       proc.on('exit', () => {
-        assert.notEqual(output, '');
-        done();
-      });
-    });
-
-  });
-
-});
+        assert.notEqual(output, '')
+        done()
+      })
+    })
+  })
+})
